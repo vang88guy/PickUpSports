8d0dc7330b73d856a028f6cea95d0cb38f156d5d
Elias Holter <eliasholter1@gmail.com>
2020-01-21 08:47:46 -0600

    Pulling updated files from chong and aaron

---
+++ PickUpSports/Models/PlayerEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PickUpSports.Models
+{
+    public class PlayerEvent
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Player")]
+        public int PlayerId { get; set; }
+        public Player Player { get; set; }
+        [ForeignKey("Event")]
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+    }
+}
